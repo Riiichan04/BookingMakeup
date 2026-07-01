c056863b4868524aec041671580f09ae283080e4
Riichan04 <22130274@st.hcmuaf.edu.vn>
2026-07-01 07:44:57 +0700

    feat(wallet): add create payment qr when withdraw wallet to service owner bank account

--- src/services/withdraw-service.ts
+++ src/services/withdraw-service.ts
@@ -33,3 +33,20 @@ export const rejectWithdraw = async (requestId: string, reason: string): Promise
     });
     return response.data;
 }
+
+//For withdraw payment
+export const generateQrUrl = (bankId: string, accountNo: string, accountName: string, amount: number, requestId: string) => {
+    const baseUrl = "https://vietqr.app/img";
+    const params = new URLSearchParams({
+        acc: accountNo,
+        bank: bankId,
+        amount: amount.toString(),
+        des: `Thanh toan rut tien ${requestId.substring(0, 8)}`,
+        template: "compact",
+        showinfo: "true",
+        fullacc: "true",
+        holder: accountName
+    });
+
+    return `${baseUrl}?${params.toString()}`;
+};
